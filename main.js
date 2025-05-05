6e0294ada64b042d9719bb4997d032bea3bee24e
Gabrieldsrod <162513291+Gabrieldsrod@users.noreply.github.com>
2025-05-05 08:02:02 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
-// Altere para o link da sua API
-const url = "https://6817c88e5a4b07b9d1cd3de9.mockapi.io/api/vi1/bateria";
+const url = "https://6817c88e5a4b07b9d1cd3de9.mockapi.io/api/vi1/bateria?sortBy=createdAt&order=desc";
 
 fetch(url)
-      .then(res => res.json())
-      .then(dados => {
-        const ultima = dados[0];
-        const tensao = parseFloat(ultima.tensao);
+  .then(res => res.json())
+  .then(dados => {
+    const ultima = dados[0];
+    const tensao = parseFloat(ultima.tensao);
 
-        const porcentagem = Math.min(100, Math.round((tensao / 13.5) * 100));
+    const porcentagem = Math.min(100, Math.round((tensao / 13.5) * 100));
 
-        const nivel = document.getElementById("nivel");
-        const texto = document.getElementById("texto");
+    const nivel = document.getElementById("nivel");
+    const texto = document.getElementById("texto");
 
-        nivel.style.width = porcentagem + "%";
+    nivel.style.width = porcentagem + "%";
 
-        if (porcentagem < 20) {
-          nivel.style.backgroundColor = "red";
-        } else if (porcentagem < 50) {
-          nivel.style.backgroundColor = "orange";
-        } else {
-          nivel.style.backgroundColor = "green";
-        }
+    if (porcentagem < 20) {
+      nivel.style.backgroundColor = "red";
+    } else if (porcentagem < 50) {
+      nivel.style.backgroundColor = "orange";
+    } else {
+      nivel.style.backgroundColor = "green";
+    }
 
-        texto.textContent = `Carga da bateria: ${porcentagem}% (${tensao.toFixed(2)} V)`;
-      })
-      .catch(err => {
-        document.getElementById("texto").textContent = "Erro ao carregar dados";
-        console.error(err);
-      });
+    texto.textContent = `Carga da bateria: ${porcentagem}% (${tensao.toFixed(2)} V)`;
+  })
+  .catch(err => {
+    document.getElementById("texto").textContent = "Erro ao carregar dados";
+    console.error(err);
+  });
